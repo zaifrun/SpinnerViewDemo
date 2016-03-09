043f72069c639571d38e57e1cfb541b897640ae1
zaifrun <martinknud@gmail.com>
2016-03-09 15:33:56 +0100

    comments

--- app/src/main/java/com/iducate/spinnerviewdemo/MainActivity.java
+++ app/src/main/java/com/iducate/spinnerviewdemo/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	//you can also put these values in xml - the book shows how.
 	private String[] items = { " Item 0", "Item 1", "Item 2", "Item 3", "Item 4",
 			"Item 5", "Item 6", "Item 7" };
 
@@ -19,6 +20,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		//The spinner is defined in our xml file
 		Spinner spinner = (Spinner) findViewById(R.id.spinner1);
 
 		//we use a predefined simple spinner drop down, 
@@ -36,6 +38,7 @@ public class MainActivity extends Activity {
 			@Override
 			public void onItemSelected(AdapterView<?> adapterView, View view,
 					int position, long id) {
+				//So this code is called when ever the spinner is clicked
 				Toast.makeText(MainActivity.this,
 						"Item selected: " + items[position], Toast.LENGTH_SHORT)
 						.show();
@@ -48,5 +51,15 @@ public class MainActivity extends Activity {
 				// or something. 
 			}
 		});
+
+		//This line of code can always be used to get the
+		//selected position in in the spinner - the first item
+		//will have an index of 0.
+		int position = spinner.getSelectedItemPosition();
+
+		//This line will get the actual seleted item -
+		//in our case the values in the spinner is simply
+		//strings, so we need to make a cast to a String
+		String item = (String) spinner.getSelectedItem();
 	}
 }
